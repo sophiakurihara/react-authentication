550d997da5f7fd8f356e4b4521991320e395a717
Sophia <sophia.sudiacal@gmail.com>
2021-01-12 15:25:51 -0600

    Protect routes requiring authentication, and implemented sign out

--- src/Context.js
+++ src/Context.js
@@ -22,7 +22,8 @@ export class Provider extends Component {
         authenticatedUser,
         data: this.data,
         actions: {
-            signIn: this.signIn
+            signIn: this.signIn,
+            signOut: this.signOut
         }
     };
     return (
@@ -46,7 +47,7 @@ export class Provider extends Component {
   }
 
   signOut = () => {
-
+    this.setState({ authenticatedUser: null });
   }
 }
 
--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -8,7 +8,12 @@ export default ({ component: Component, ...rest }) => {
       { context => (
         <Route
           {...rest}
-          render={}
+          render={props => context.authenticatedUser ? (
+              <Component {...props} />
+            ) : (
+              <Redirect to="/signin" />
+            )
+          }
         />
       )}
     </Consumer>
